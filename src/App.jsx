f1e109f2a7d0b8e190d0b7c95a12c73604534a53
quantumCode <quantumcode22@gmail.com>
2025-07-08 20:06:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <div>
+        {/* -------Navbar-------- */}
+        {/* -------Hero Grid-------- */}
+      </div>
+      {/* Some absolute elements */}
+      {/* ---------BG_comp---------- */}
+    </div>
+  );
+}
+
+export default App;
