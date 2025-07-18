a5bab513f057361ea0bdc7b2db8f1cc888d7e656
quantumCode <quantumcode22@gmail.com>
2025-07-18 07:57:33 +0530

    deployment and compatibility related fixes

--- src/components/MetalBorderWrapper.jsx
+++ src/components/MetalBorderWrapper.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import NoiseBox from "./SVGs/NoiseBox";
 import { motion } from "motion/react";
 
+const maskStyle = {
+  background: "#00000000",
+  mask: "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+  maskComposite: "subtract",
+  WebkitMask:
+    "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+  WebkitMaskComposite: "source-out",
+  padding: "2px",
+};
 const MetalBorderWrapper = ({ children, isActive, borderRadius = 0 }) => {
   return (
     <div className="relative inset-0 p-[4px]">
@@ -10,8 +19,8 @@ const MetalBorderWrapper = ({ children, isActive, borderRadius = 0 }) => {
         animate={isActive ? { opacity: 1 } : { opacity: 0 }}
         transition={{ ease: "easeIn", duration: 0.2 }}
         exit={{ opacity: 0 }}
-        style={{ borderRadius: borderRadius }}
-        className="absolute inset-0 noise-border w-full h-full overflow-hidden flex justify-center items-center z-100"
+        style={{ ...maskStyle, borderRadius: borderRadius }}
+        className="absolute inset-0 w-full h-full overflow-hidden flex justify-center items-center z-100"
       >
         <NoiseBox />
       </motion.div>
--- src/components/MetalBorderWrapperHover.jsx
+++ src/components/MetalBorderWrapperHover.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import NoiseBox from "./SVGs/NoiseBox";
 import { motion } from "motion/react";
+const maskStyle = {
+  background: "#00000000",
+  mask: "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+  maskComposite: "subtract",
+  WebkitMask:
+    "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+  WebkitMaskComposite: "source-out",
+  padding: "2px",
+};
 
 const MetalBorderWrapperHover = ({
   children,
@@ -11,8 +20,8 @@ const MetalBorderWrapperHover = ({
   return (
     <div style={{ padding: padding }} className="relative h-fit w-fit inset-0">
       <motion.div
-        style={{ borderRadius: borderRadius }}
-        className="absolute inset-0 noise-border w-full h-full overflow-hidden flex justify-center items-center p-[2px] z-100 hover:opacity-80 opacity-0 transition-opacity"
+        style={{ ...maskStyle, borderRadius: borderRadius }}
+        className="absolute inset-0 w-full h-full overflow-hidden flex justify-center items-center p-[2px] z-100 hover:opacity-80 opacity-0 transition-opacity"
       >
         <NoiseBox noiseSize={noiseSize} />
       </motion.div>
