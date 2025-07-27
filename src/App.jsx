b0afc4d7d773d17a667cd64e9ce1aa620eba378e
quantumCode <quantumcode22@gmail.com>
2025-07-27 20:56:37 +0530

    fixed zoom lvls

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [activeBgImg, setActiveBgImg] = useState(0);
 
   return (
-    <div className="">
+    <div className="w-screen h-screen">
       <div className="absolute inset-0 flex flex-col inter-font">
         <div className="relative h-fit inset-0 mx-[5rem] pt-8 z-50 flex flex-col gap-[2rem] no-scroll">
           {/* -------Navbar-------- */}
@@ -32,7 +32,7 @@ function App() {
       <BgComp />
       <div className="fixed w-full h-full bg-gray-900 z-200 md:hidden flex justify-center items-center ">
         <div className="text-2xl text-gray-600 font-mono">
-          {"Please open this website on bigger screen :-)"}
+          {"Please open this website on a bigger screen :-)"}
         </div>
       </div>
     </div>
